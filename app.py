20f89300c09d74c8005bcac9f67f9e58107a7507
Wanderson_Pereira <wanderson.p.ayres@gmail.com>
2026-06-16 11:27:28 -0300

    LLM loading

---
+++ app.py
@@ -0,0 +1,29 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+load_dotenv()
+
+st.set_page_config(page_title="Atendimento SafeBank 🤖", page_icon="🤖")
+st.title("Atendimento SafeBank")
+
+id_model = "llama-3.3-70b-versatile"
+temperature = 0.7
+path = "content"
+
+### Carregamento da LLM
+def load_llm(id_model, temperature):
+  llm = ChatGroq(
+    model=id_model,
+    temperature=temperature,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+  )
+  return llm
+
+llm = load_llm(id_model, temperature)
